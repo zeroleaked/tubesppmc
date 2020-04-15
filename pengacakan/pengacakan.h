3b54312021220f19e540d49165ddd20e86eddf18
gintkenny <gintkenny@gmail.com>
2020-04-15 15:14:53 +0700

    update pengacakan

--- pengacakan/pengacakan.h
+++ pengacakan/pengacakan.h
@@ -4,7 +4,7 @@
 
 arrKata getRandomKey(arrKata *dict, int n)
 {
-    arrKata *temp, toReturn;
+    arrKata temp, toReturn;
     int index, dictLength, currRow = 0, i;
     temp = dict;
 
@@ -14,18 +14,14 @@ arrKata getRandomKey(arrKata *dict, int n)
     index = rand()%dictLength;
 
     //menuju ke row yang diinginkan (hasil random)
-    while(currRow < index)
-    {
-        temp = temp->next;
-        currRow += 1;
-    }
+    temp = peekDictionary(dict, index);
 
     //sudah di Row yang diinginkan, assign nilai-nilai untuk return
     toReturn.next = NULL;
     toReturn.length = n;
     for ( i = 0; i < n; i++)
     {
-        pushArrKata(&toReturn, temp->array[i]);
+        pushArrKata(&toReturn, temp.array[i]);
     }
     
     return toReturn;
