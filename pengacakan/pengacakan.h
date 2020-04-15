382ec0403cef6e87085a3695a6c2df1434a8a6da
michaelkresna02 <60211874+michaelkresna02@users.noreply.github.com>
2020-04-15 23:36:23 +0700

    Update pengacakan.h

--- pengacakan/pengacakan.h
+++ pengacakan/pengacakan.h
@@ -1,12 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
-#include "konfigurasi.h"
+#include "../konfigurasi/konfigurasi.h"
 
 arrKata getRandomKey(arrKata *dict, int n)
 {
     arrKata temp, toReturn;
     int index, dictLength, currRow = 0, i;
-
     srand(time(0));
 
     dictLength = getDictionaryLength(dict);
@@ -16,8 +15,7 @@ arrKata getRandomKey(arrKata *dict, int n)
     temp = peekDictionary(dict, index);
 
     //sudah di Row yang diinginkan, assign nilai-nilai untuk return
-    toReturn.next = NULL;
-    toReturn.length = n;
+    initializeArrKata(&toReturn);
     for ( i = 0; i < n; i++)
     {
         pushArrKata(&toReturn, temp.array[i]);
@@ -25,10 +23,11 @@ arrKata getRandomKey(arrKata *dict, int n)
     
     return toReturn;
 }
-
-char* randomWord(arrKata *value, int n){
-    int indeks;
-    indeks= rand()%((value->length)-n)-1;
-    char word=value.array[indeks+n];
-    return (&word);
+char* randomWord(arrKata *value){
+    int index;
+    srand(time(0));
+    index= rand()%(value->length);
+    char *word;
+    word = value->array[index];
+    return (word);
 }
