3e5e482f1affe0fee1dedacec06ce70b1810a81d
zir <fakegags@gmail.com>
2020-04-17 05:34:59 +0700

    initial

--- main.c
+++ main.c
@@ -1,90 +1,221 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <string.h>
-#include "pengacakan/pengacakan.h"
-#include "lookup/lookup.h"
-#include "konfigurasi/konfigurasi.h"
-#include "input/input.h"
-#include "flow/flow.h"
+#include <time.h>
 
+#define MAX_WORD_LENGTH 32
 
-int main()
+typedef struct node{
+  char word[MAX_WORD_LENGTH];
+  struct node *next;
+  int child_length;
+  struct node *child;
+  struct node *sibling;
+} node;
+
+node *addToEmpty(node *last, char word[MAX_WORD_LENGTH])
 {
-    //deklarasi variabel
-    arrKata *dict, *inText, key, *value;
-    char *outVal;
-    int n, banyak_kata, printed = 0;
-    time_t t;
+    // This function is only for empty list
+    if (last != NULL)
+      return last;
 
-    //set seed utk random
-    srand((unsigned) time(&t));
-    printf("                            Welcome to N-Gram Simulation\n");
-    printf("                                Delivered by Group C6\n");
-    printf("\n");
-    printf("__________________________________________________________________________________________\n");
-    printf("\n");
-    printf("1.Kendrik Emkel Ginting 13218077\n");
-    printf("2.Rizki Habibi Alamsyah 13218080\n");
-    printf("3.Mohammad Salman Subki 13218078\n");
-    printf("4.Anindhita Nayazirly   13218083\n");
-    printf("5.Michael Kresna Putra  13218066\n");
-    printf("\n");
-    printf("Dalam dunia computational linguistics dan probabilitas dikenal istilah n-gram, yaitu rangkaian\nkata-kata, fonem, huruf, suku kata, atau yang lainnya dari sebuah teks atau kalimat. Model\nn-gram merupakan pendekatan bahasa menggunakan konsep probabilitas untuk memprediksi atau\nmenebak kata selanjutnya dari suatu rangkaian kata-kata.\n");
-    printf("\n");
-    int isEnd=0;
+    // Creating a node dynamically.
+    last = (node*)malloc(sizeof(node));
 
-    while(isEnd==0){
-        char End[5];
-        //insialisasi arrKata inText
-        initializeArrKata(&inText);
-        initializeArrKata(&value);
+    // Assigning the word.
+    strcpy(last -> word, word);
 
-        //User input text, value of n, banyak kata yang ingin dicetak
-        inputText(inText);
-        inputN(&n);
-        inputBanyakKata(&banyak_kata);
-        
-        //membuat dictionary
-        createDictionary(n, *inText, &dict);
+    // Note : list was empty. We link single node
+    // to itself.
+    last -> next = last;
+    last -> sibling = NULL;
+    last -> child_length = 1;
 
-        //mengambil key pertama secara random
-        key = getRandomKey(dict, n);
+    return last;
+}
 
-        //mencetk n kata pertama (key pertama)
-        while (printed < n)
-        {
-            printf("%s ", key.array[printed]);
-            printed += 1;
-        }
-        
-        // pada tahap ini printed = n, printed adalah counter berapa banyak yg sudah dicetak
+node *addEnd(node *last, char word[MAX_WORD_LENGTH])
+{
+    if (last == NULL)
+        return addToEmpty(last, word);
 
-        //memperoleh value berdasarkan key (fungsi blm terdefinisi)
-        lookupDictionary(dict, n, key, &value);
+    node *temp = (node *)malloc(sizeof(node));
 
-        //merandom value yang akan dicetak
-        outVal = randomWord(value);
+    strcpy(temp -> word, word);
+    temp -> sibling = NULL;
+    temp -> child_length = 1;
+    temp -> next = last -> next;
+    last -> next = temp;
+    last = temp;
 
-        //mencetak outVal (salah satu value dari key sebelumnya), increment counter
-        printf("%s ", outVal);
-        printed += 1;
+    return last;
+}
 
-        //append outVal ke dalam key
-        pushArrKata(&key, outVal);
-        //geser head key->array[0] = key->array[1]
-        deleteHeadArrKata(&key);
 
-        //looping hingga printed = banyak_kata
-        Flow(key, dict, n, printed, banyak_kata);
+void freeList(node *head)
+{
+   node *tmp;
 
-        printf("Apakah Anda Ingin Melakukan Pembacaan Lagi? (Yes/No)\n");
-        scanf("%s", End);
-        printf("\n");
-        
-        if (strcmp("Yes", End)){
-            isEnd=1;
-        }
+   while (head != NULL)
+    {
+       tmp = head;
+       head = head->next;
+       free(tmp);
     }
-    return 0;
+
+}
+
+// false if not same, true if same;
+int compareTwoNodes(node *a, node *b, int n) {
+  // printf("comparing [%s %s] [%s %s]\n", a->word, a->next->word, b->word, b->next->word);
+  for (int i = 0; i < n; i++) {
+    if (strcmp(a->word, b->word)) return 0;
+    a = a->next;
+    b = b->next;
+  }
+  return 1;
+}
+
+// find first match
+node *findMatch(node *a, int n, int length) {
+  node *b = a;
+  for (int i = 1; i < length; i++) {
+    if (compareTwoNodes(a, b->next, n)) {
+      // printf("a = [%s %s], b = [%s %s]\n", a->word, a->next->word, b->next->word, b->next->next->word);
+      return b;
+    }
+    b = b->next;
+  }
+  return NULL;
+}
+
+void addSibling(node *a, node *b) {
+  if (a->sibling == NULL) {
+    a->sibling = b;
+    return;
+  }
+  addSibling(a->sibling, b);
+}
+
+void createTree(node *head, int length, int n) {
+  node *a = head;
+  for (int i = 0; i < length; i++) {
+    node *b = findMatch(a, n, length - i);
+    if (b != NULL) {
+      // printf("b = [%s %s %s]\n", b->word, b->next->word, b->next->next->word);
+      // change b child to a
+      // printf("a = %s\n", a->word);
+      b->child = a;
+      // add b_value to a_value sibling
+      node *a_value = a, *b_value = b->next;
+      for (int j = 0; j < n; j++) {
+        a_value = a_value->next;
+        b_value = b_value->next;
+      }
+      // printf("a_value = %s, b_value = %s\n", a_value->word, b_value->word);
+      addSibling(a_value, b_value);
+    }
+    a = a->next;
+  }
+  printf("finished building tree\n");
+}
+
+node *countChild(node *head, int length) {
+  for (int i = 0; i < length; i++) {
+    int count = 0;
+    node *child = head->child;
+    while(child != NULL) {
+      count++;
+      child = child->sibling;
+    }
+    head->child_length = count;
+    head = head->next;
+  }
+}
+
+node *getChild(node *parent, int index) {
+  node *child = parent->child;
+  for (int i = 0; i < index; i++)
+    child = child->sibling;
+  return child;
+}
+
+node *getRandomNode(node *head, int length) {
+  int index = (rand() % length);
+  for (int i = 0; i < index; i++) {
+    head = head->next;
+  }
+  return head;
+}
+
+int main() {
+  // user input
+  int n = 2;
+  int print_length = 100;
+  char *infile = "mobydick.txt";
+  char *outfile = "outfile.txt";
+
+
+  // scan text
+  FILE *fptr = fopen(infile, "r");
+  char temp[MAX_WORD_LENGTH];
+
+  node *last = NULL;
+  int length = 0;
+  while (fscanf(fptr, "%s", temp) != EOF) {
+    last = addEnd(last, temp);
+    length++;
+  }
+  printf("length = %d\n", length);
+
+  // copy next to child
+  for (int i = 0; i < length; i++) {
+    last->child = last->next;
+    last = last->next;
+  }
+
+  // create tree
+  createTree(last->next, length, n);
+  countChild(last->next, length);
+
+  // mainkeun
+  printf("printing...\n");
+  srand(time(0));
+  int max;
+  node *print = getRandomNode(last, length);
+  fptr = fopen(outfile, "w");
+  fprintf(fptr, "... ");
+  for (int i = 0; i < print_length; i++) {
+    fprintf(fptr, "%s ", print->word);
+    max = print->child_length;
+    int num = (rand() % max);
+    print = getChild(print, num);
+  }
+  fprintf(fptr, "...");
+  printf("finished printing\n");
+
+  #ifdef DEBUG
+  // ini nge print semua node
+  last = last->next;
+  for (int i = 0; i < length; i++) {
+    printf("%s %d\n", last->word, last->child_length);
+    printf("child = ");
+    node *child = last->child;
+
+    for (int j = 0; j < last->child_length; j++) {
+      printf("%s ", child->word);
+      child = child->sibling;
+    }
+    printf("\n");
+
+    last = last->next;
+  }
+  printf("\n");
+  #endif
+
+  // unlink circular list
+  node *head = last -> next;
+  last->next = NULL;
+
+  freeList(head);
 }
