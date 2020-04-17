3804a307cb67daf603038015fbcce2113c90c7b3
salmansubki <63189708+salmansubki@users.noreply.github.com>
2020-04-17 17:57:49 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 /* EL2208 Praktikum Pemecahan Masalah dengan C 2019/2020
 * MODUL 8 – TUGAS BESAR
 * Kelompok : C6
-* Hari dan Tanggal : Jumat, 17 April 2018
+* Hari dan Tanggal : Jumat, 17 April 2020
 * Asisten (NIM) : Sarah Az Zahra (13216099)
 * Nama File : main.c
 * Deskripsi : Program utama untuk dijalankan
